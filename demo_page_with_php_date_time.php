7018b78948368ab724c6fba5c19192aa426e0bbf
alexandrabu <161487129+alexandrabu@users.noreply.github.com>
2024-03-01 18:53:51 +0200

    demo_page_with_php_date_time.php

---
+++ demo_page_with_php_date_time.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+</head>
+<body>
+<h1>
+    <?php
+    echo "Pagina demo!";
+    ?>
+</h1>
+
+<p>
+    <?php
+
+    echo "Data și ora  sunt: " . date("Y-m-d H:i:s");
+    ?>
+</p>
+
+
+</body>
+</html>
