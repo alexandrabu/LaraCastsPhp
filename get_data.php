6d1ea758ba790027073c43997098d6253a11c947
alexandrabu <161487129+alexandrabu@users.noreply.github.com>
2024-03-04 14:52:39 +0200

    get_data.php

---
+++ get_data.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$database = "database_name";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM your_table";
+$result = $conn->query($sql);
+
+$data = array();
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+}
+
+echo json_encode($data);
+
+$conn->close();
+?>
